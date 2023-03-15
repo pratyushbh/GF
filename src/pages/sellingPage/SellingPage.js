2dd9c02a7d9806d05e5fe87f23172b40b48db893
Pratyush Bhatnagar <38534434+pratyushbh@users.noreply.github.com>
2023-03-15 12:12:41 +0530

    formatted the backend data

--- src/pages/sellingPage/SellingPage.js
+++ src/pages/sellingPage/SellingPage.js
@@ -4,11 +4,21 @@ import axios from 'axios'
 
 export default function SellingPage() {
   const [img,setImg]=useState('');
+  const [link,setLink]=useState('');
+  const [nftname,setNftname]=useState('');
+  const [desc,setDesc]=useState('');
   function upload(event){
     console.log(event.target.files)
     setImg(event.target.files[0]);
+    console.log(link);
     }
-  const SubmitImg=()=>{
+  function handleName(e){
+    setNftname(e.target.value);
+  }
+  function handleDesc(e){
+    setDesc(e.target.value);
+  }
+  const Submit=()=>{
     console.log(img);
     const data=new FormData();
     data.append("file",img)
@@ -19,8 +29,12 @@ export default function SellingPage() {
       body:data
     })
     .then(res=>{return res.json()})
-    .then(data=>{console.log(data.url)})
-    .catch(err=>{console.log(err)})
+    .then(async data=>{
+      await console.log(data.url);
+      const Bdata = await {name:nftname,desc:desc,imgLink:data.url};
+      console.log(Bdata);
+    })
+    .catch(err=>{console.log(err.message)})
   }
   return (
     <>
@@ -35,14 +49,14 @@ export default function SellingPage() {
         <div>
           <h3>Enter NFT Title</h3>
           <br/>
-          <input type="text"></input>
+          <input type="text" onChange={handleName} ></input>
         </div>
         <div className="ehh">
           <h3>Enter NFT Description</h3>
-          <textarea placeholder="A collection of 555 bored apes"></textarea>
+          <textarea onChange={handleDesc} placeholder="A collection of 555 bored apes"></textarea>
         </div>
       </form>
-      <button onClick={SubmitImg} className="submit-btn">Submit</button>
+      <button onClick={Submit} className="submit-btn">Submit</button>
       </div>
       </div>
     </>
